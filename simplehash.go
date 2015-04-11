55297237124bfcb2bbfaca143437ef604f6a75a2
Elvin Yung <elvinyung1@gmail.com>
2015-04-11 06:54:37 -0400

    Add collision check

--- simplehash.go
+++ simplehash.go
@@ -1,25 +1,34 @@
 package gash
 
 type SimpleHash struct {
-    items []KvPair
+    items [][]KvPair
     capacity int
 }
 
 func Create(capacity int) SimpleHash {
     table := SimpleHash{}
     table.capacity = capacity
-    table.items = make([]KvPair, capacity)
+    table.items = make([][]KvPair, capacity)
+    for index := range table.items {
+        table.items[index] = []KvPair{}
+    }
+
     return table
 }
 
 func (table SimpleHash) Insert(k string, v interface{}) {
     index := Djb2(k) % table.capacity
     pair := KvPair{k, v}
-    table.items[index] = pair
+    table.items[index] = append(table.items[index], pair)
 }
 
 func (table SimpleHash) Find(k string) interface{} {
     index := Djb2(k) % table.capacity
-    pair := table.items[index]
-    return pair.Value
+    var result KvPair
+    for _, pair := range table.items[index] {
+        if pair.Key == k {
+            result = pair
+        }
+    }
+    return result.Value
 }
