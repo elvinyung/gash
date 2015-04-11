e9c1f8c005129e9a882689da4c16c23406e5b2e5
Elvin Yung <elvinyung1@gmail.com>
2015-04-11 06:54:26 -0400

    Fix hash function to actually hash by character value

--- hashfns.go
+++ hashfns.go
@@ -5,8 +5,8 @@ package gash
 // a simple djb2 implementation
 func Djb2(s string) int {
     hash := 5381
-    for c := range s {
-        hash += (hash * 33) + c
+    for _, c := range s {
+        hash += (hash * 33) + int(c)
     }
 
     return hash
