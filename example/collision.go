a1c59941889467908bf3bad1a180029a70305d5f
Elvin Yung <elvinyung1@gmail.com>
2015-04-12 08:24:14 -0400

    Add example for key value mutation

---
+++ example/collision.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "github.com/elvinyung/gash"
+)
+
+func main() {
+    p := gash.KvPair{"samplekey", "samplevalue"}
+    fmt.Println(p.Key)
+
+    t := gash.Create(1000)
+    t.Insert("foo", 4)
+    fmt.Println(t.Find("foo"))
+    t.Insert("foo", "asdfofsdja")
+    fmt.Println(t.Find("foo"))
+}
